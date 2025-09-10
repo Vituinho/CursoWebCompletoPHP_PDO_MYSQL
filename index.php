8e5fbdac05c46b1a356bac34198aad2aeac70951
Vituinho <trabalhovictor531@gmail.com>
2025-09-10 09:15:12 -0300

    Feat: Criando uma conexão entre o PHP e o MySQL com PDO, Tratando exceptions, Executando instruções SQL.

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+    $usuario = 'root';
+    $senha = '';
+
+    try {
+        $conexao = new PDO($dsn, $usuario, $senha);
+
+        $query = '
+            create table if not exists tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null
+            )
+        ';
+
+        $retorno = $conexao->exec($query);
+        //0
+
+        echo $retorno;
+
+        $query = '
+            delete from tb_usuarios
+        ';
+
+        $retorno = $conexao->exec($query);
+        echo $retorno;
+
+    } catch(PDOException $e) {
+        echo 'erro: ' . $e->getCode(). ' Mensagem: '.$e->getMessage();
+        //registrar erro
+
+    }
+
+    
